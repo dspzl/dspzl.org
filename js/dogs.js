800257dc5706f97d72812a97fbcb089a27bbc676
Carson Meyer <meyerc1@rose-hulman.edu>
2021-01-01 17:15:00 -0500

    Added dogs page and dogs.js based on brothers page and brothers.js

---
+++ js/dogs.js
@@ -0,0 +1,34 @@
+var dogs = [{ name: "Piper", hometown: "Fishers, IN", major: "Fetch", username: "piper" },
+            { name: "Leo", hometown: "St. Louis, MO", major: "Sleeping", username: "leo"}
+];
+
+function getDogsCard(dog){
+  var html = '<div class="col l6">';
+  html += '<div class="card-panel z-depth-1">';
+  html += '<div class="row valign-wrapper no-margin">';
+  html += '<div class="col s3">';
+  html += '<img id="' + dog.username + '" src="img/portraits/'+dog.username+'.jpg" alt="" class="circle responsive-img valign">';
+  html += '</div>';
+  html += '<div class="col s9">';
+  html += '<h5 class="no-top-margin"><strong>'+dog.name+'</strong></h5>';
+  html += '<h6 class="no-margin">'+dog.hometown+'</h6>';
+  html += '<h6>'+dog.major+'</h6>';
+  html += '</div>';
+  html += '</div>';
+  html += '</div>';
+  html += '</div>';
+  return html;
+}
+
+
+//add dogs
+for(var i=0; i<dogs.length; i+=2){
+  var html = '<div class="row content no-margin">';
+  html += getDogsCard(dogs[i]);
+  if( (i+1) < dogs.length){
+    html += getDogsCard(dogs[i+1]);
+  }
+  html += '</div>';
+  html += '</div>';
+  $("#dogs-container").append(html);
+}
