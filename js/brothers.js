86d7b894e617e05957d4e8d497323fda33fc22eb
DavidPurdy1 <Purdydj@Rose-Hulman.edu>
2022-09-09 10:20:47 -0500

    Fall 2022 Update

--- js/brothers.js
+++ js/brothers.js
@@ -1,70 +1,56 @@
+/* Update the brothers list here */
+
 var seniors = [
-    { name: "Evan Thomas Baker", hometown: "Springville, IN", major: "Mechanical Engineering", username: "bakeret"},
-    // { name: "Nicholas Paul Barr", hometown: "Fishers, IN", major: "Mechanical Engineering", username: "barrnp" },
-    { name: "Calen Michael Cook", hometown: "Brazil, IN", major: "Chemical Engineering", username: "cookc4" },
-    { name: "Lucas Alberto D'Alesio", hometown: "Park Ridge, IL", major: "Computer Engineering", username: "dalesil" },
-    // { name: "Songwei Fan", hometown: "Shenzhen, China", major: "Mechanical Engineering", username: "fans" },
-    { name: "Bryce Benjamin Hatfield", hometown: "Saint John, IN", major: "Chemical Engineering", username: "hatfiebb" },
-    { name: "Nasser Nadim Hegar", hometown: "Noblesville, IN", major: "Computer Engineering", username: "hegarnn" },
-    { name: "Brian Michael Huyge", hometown: "Marshall, MI", major: "Chemical Engineering", username: "huygebm" },
-    { name: "Michael James Johnson", hometown: "Gothenburg, Sweden", major: "Mechanical Engineering", username: "johnsom4" },
-    // { name: "Carson James Meyer", hometown: "St. Louis, MO", major: "Computer Engineering", username: "meyerc1" },
-    { name: "Aidan Cory Moss", hometown: "Broomfield, CO", major: "Computer Engineering", username: "mossac" },
-    { name: "Haiden Michael Smith", hometown: "Manhattan, IL", major: "Electrical Engineering", username: "smithhm" },
-    { name: "Jacob Dietrich Tebbe", hometown: "Lebanon, OH", major: "Electrical Engineering", username: "tebbejd" },
-    { name: "Tyler Joe Thenell", hometown: "West Milford, NJ", major: "Computer Engineering", username: "theneltj" },
-    { name: "Geoffrey Barton Tomlinson", hometown: "Houston, TX", major: "Computer Engineering", username: "tomlingb" },
-    { name: "Robert Dilbert DeBerry", hometown: "Farm, IN", major: "Mechanical Engineering", username: "deberrr"},
-    { name: "William Woke McEvoy", hometown: "Chicago, IL", major: "Computer Science", username: "mcevoywj"},
-    { name: "Joshua DoggyDog Warning", hometown: "Noblesville, IN", major: "Software Engineering", username: "warninjd"},
-    { name: "Griffin John Altmix", hometown: "Cincinnati, OH", major: "Mechanical Engineering", username: "altmixgj"},
-    { name: "Mark Andrew Gearhart Jr.", hometown: "Hobart, IN", major: "Chemical Engineering", username: "gearhama"},
-  ];
+  { name: "Nasser Nadim Hegar", hometown: "Noblesville, IN", major: "Computer Engineering", username: "hegarnn" },
+  { name: "Brian Michael Huyge", hometown: "Marshall, MI", major: "Chemical Engineering", username: "huygebm" },
+  { name: "Michael James Johnson", hometown: "Gothenburg, Sweden", major: "Mechanical Engineering", username: "johnsom4" },
+  { name: "Vance Joseph Allen", hometown: "San Diego, CA", major: "Mechanical Engineering", username: "allenvj" },
+  { name: "William Bartlett Bowman", hometown: "Germantown, OH", major: "Mathematics", username: "bowmanwb" },
+  { name: "Sang Heon Choi", hometown: "Sungnam, South Korea", major: "Computer Science", username: "chois3" },
+  { name: "John 'Yoon Su' Chung", hometown: "Santa Clara, CA", major: "Engineering Design", username: "chungy" },
+  { name: "Brendon Michael Crabtree", hometown: "Indianapolis, IN", major: "Mechanical Engineering", username: "crabtrbm" },
+  { name: "Ethan Benjamin Drong", hometown: "Lockport, IL", major: "Mechanical Engineering", username: "drongeb" },
+  { name: "Jameson Michael Jochheim", hometown: "Round Lake, IL", major: "Mechanical Engineering", username: "jochhejm" },
+  { name: "Samuel Edward Johnson", hometown: "Oldenburg, IN", major: "Mechanical Engineering", username: "johnsose" },
+  { name: "Joshua Steven Mitterling", hometown: "Noblesville, IN", major: "Computer Engineering", username: "mitterj" },
+  { name: "Aidan Drew Morris", hometown: "Richmond, IN", major: "Chemical Engineering", username: "morrisad" },
+  { name: "Collin Mao Morris", hometown: "Shanghai, China", major: "Computer Science", username: "morrisc1" },
+  { name: "David Jason Purdy", hometown: "Franklin, IN", major: "Computer Science", username: "purdydj" },
+  { name: "Carl Frederick Quist", hometown: "Marshall, MI", major: "Mechanical Engineering", username: "quistcf" },
+  { name: "Jack Phillip Raker", hometown: "Palatine, IL", major: "Chemical Engineering", username: "rakerjp" },
+  { name: "Mason Darrell Reid", hometown: "Glasgow, KY", major: "Biology", username: "reidmd" },
+  { name: "Luke Abraham Wendel", hometown: "Noblesville, IN", major: "Computer Engineering", username: "wendella" },
+  { name: "Ethan Douglas Whetter", hometown: "Santa Barbara, CA", major: "Biomedical Engineering", username: "whetteed" }
+];
 
 var juniors = [
-    {name: "Vance Joseph Allen", hometown: "San Diego, CA", major: "Mechanical Engineering", username: "allenvj"},
-    {name: "William Bartlett Bowman", hometown: "Germantown, OH", major: "Mathematics", username: "bowmanwb"},
-    {name: "Sang Heon Choi", hometown: "Sungnam, South Korea", major: "Computer Science", username: "chois3"},
-    {name: "John 'Yoon Su' Chung", hometown: "Santa Clara, CA", major: "Engineering Design", username: "chungy"},
-    {name: "Brendon Michael Crabtree", hometown: "Indianapolis, IN", major: "Mechanical Engineering", username: "crabtrbm"},
-    {name: "Ethan Benjamin Drong", hometown: "Lockport, IL", major: "Mechanical Engineering", username: "drongeb"},
-    {name: "Jameson Michael Jochheim", hometown: "Round Lake, IL", major: "Mechanical Engineering", username: "jochhejm"},
-    {name: "Samuel Edward Johnson", hometown: "Oldenburg, IN", major: "Mechanical Engineering", username:"johnsose"},
-    {name: "Joshua Steven Mitterling", hometown: "Noblesville, IN", major: "Computer Engineering", username: "mitterj"},
-    {name: "Aidan Drew Morris", hometown: "Richmond, IN", major: "Chemical Engineering", username: "morrisad"},
-    {name: "Collin Mao Morris", hometown: "Shanghai, China", major: "Computer Science", username: "morrisc1"},
-    {name: "David Jason Purdy", hometown: "Franklin, IN", major: "Computer Science", username: "purdydj"},
-    {name: "Carl Frederick Quist", hometown: "Marshall, MI", major: "Mechanical Engineering", username: "quistcf"},
-    {name: "Jack Phillip Raker", hometown: "Palatine, IL", major: "Chemical Engineering", username: "rakerjp"},
-    {name: "Mason Darrell Reid", hometown: "Glasgow, KY", major: "Biology", username: "reidmd"},
-    {name: "Luke Abraham Wendel", hometown: "Noblesville, IN", major: "Computer Engineering", username:"wendella"},
-    {name: "Ethan Douglas Whetter", hometown: "Santa Barbara, CA", major: "Biomedical Engineering", username: "whetteed"}
+  { name: "Richard Wayne Barker III", hometown: "Columbia, IL", major: "Engineering Design", username: "barkerrw" },
+  { name: "Aidan Jeffery Sturgeon", hometown: "West Lafayette, IN", major: "Mechanical Engineering", username: "sturgeaj" },
+  { name: "Mason Steven Wykes", hometown: "Greenville, OH", major: "Mechanical Engineering", username: "wykesms" },
 ];
 
 var sophomores = [
-  {name: "Richard Wayne Barker III", hometown: "Columbia, IL", major: "Engineering Design", username: "barkerrw"},
-  {name: "Aidan Jeffery Sturgeon", hometown: "West Lafayette, IN", major: "Mechanical Engineering", username: "sturgeaj"},
-  {name: "Mason Steven Wykes", hometown: "Greenville, OH", major: "Mechanical Engineering", username: "wykesms"},
+  { name: "Ziyu 'Leonardo' Xie", hometown: "Chengdu City, China", major: "Computer Engineering", username: "xiez6" },
+  { name: "Ian 'Earl' Christopher Resnik", hometown: "Pittsburgh, PA", major: "Mechanical Engineering", username: "resnikic" },
+  { name: "Nikita Romanovich Egorov", hometown: "Fishers, IN", major: "Electrical Engineering", username: "egorovn" },
+  { name: "Joshua Quinn Shrock", hometown: "Goshen, IN", major: "Computer Engineering", username: "shrocjq" },
+  { name: "Aidan Donald Janc", hometown: "Chicago, IL", major: "Computer Science", username: "jancad" },
+  { name: "Ian Henry Stedham", hometown: "Columbia, MD", major: "Computer Science", username: "stedhai" },
+  { name: "Adam Steven Deckard", hometown: "Nashville, TN", major: "Mechanical Engineering", username: "deckaras" },
+  { name: "Nathan Rupert Pratt", hometown: "Hopewell, NJ", major: "Eng. Physics", username: "prattnr" },
+  { name: "Zachary ZheYing Cao", hometown: "Louisville, KY", major: "Computer Science", username: "caozz" },
+  { name: "Andrew Dietrich Nichols", hometown: "Houston, TX", major: "Mechanical Engineering", username: "nicolad" },
+  { name: "Apollo Jackson Picot", hometown: "Rockton, IL", major: "Mechanical Engineering", username: "picotaj" },
+  { name: "Damian Ivan Dalic", hometown: "Chicago, IL", major: "Computer Science", username: "dalicdi" },
+  { name: "Matthew Li Hart", hometown: "Clarksville, MD", major: "Electrical Engineering", username: "hartml" },
+  { name: "Drew Ryan Kilner", hometown: "Louisville, KY", major: "Computer Engineering", username: "kilnerdr" },
+  { name: "James 'Zeno' Day", hometown: "Lexington, KY", major: "Computer Science", username: "dayjz" },
+  { name: "Anuj Suvarna", hometown: "St. Charles, IL", major: "Computer Engineering", username: "suvarna1" },
+  { name: "Vincent Donovan Hammer", hometown: "Noblesville, IN", major: "Optical Engineering", username: "hammervd" },
+  { name: "Junki Lee", hometown: "Montrose, CA", major: "Computer Engineering", username: "leej25" },
 ];
 
-var freshman = [
-  { name: "Ziyu 'Leonardo' Xie", hometown: "Chengdu City, China", major: "Computer Engineering", username: "xiez6"},
-  { name: "Ian 'Earl' Christopher Resnik", hometown: "Pittsburgh, PA", major: "Mechanical Engineering", username: "resnikic"},
-  { name: "Nikita Romanovich Egorov", hometown: "Fishers, IN", major: "Electrical Engineering", username: "egorovn"},
-  { name: "Joshua Quinn Shrock", hometown: "Goshen, IN", major: "Computer Engineering", username: "shrocjq"},
-  { name: "Aidan Donald Janc", hometown: "Chicago, IL", major: "Computer Science", username: "jancad"},
-  { name: "Ian Henry Stedham", hometown: "Columbia, MD", major: "Computer Science", username: "stedhai"},
-  { name: "Adam Steven Deckard", hometown: "Nashville, TN", major: "Mechanical Engineering", username: "deckaras"},
-  { name: "Nathan Rupert Pratt", hometown: "Hopewell, NJ", major: "Eng. Physics", username: "prattnr"},
-  { name: "Zachary ZheYing Cao", hometown: "Louisville, KY", major: "Computer Science", username: "caozz"},
-  { name: "Andrew Dietrich Nichols", hometown: "Houston, TX", major: "Mechanical Engineering", username: "nicolad"},
-  { name: "Apollo Jackson Picot", hometown: "Rockton, IL", major: "Mechanical Engineering", username: "picotaj"},
-  { name: "Damian Ivan Dalic", hometown: "Chicago, IL", major: "Computer Science", username: "dalicdi"},
-  { name: "Matthew Li Hart", hometown: "Clarksville, MD", major: "Electrical Engineering", username: "hartml"},
-  { name: "Drew Ryan Kilner", hometown: "Louisville, KY", major: "Computer Engineering", username: "kilnerdr"},
 
-
-];
 
 function getBrotherCard(brother){
   var email = brother.username + '@rose-hulman.edu';
@@ -87,6 +73,7 @@ function getBrotherCard(brother){
   `;
 }
 
+// For 1 brother card
 function buildContainer(brotherList, selector){
   var html = "";
   for(var i = 0; i < brotherList.length; i += 2){
@@ -100,13 +87,50 @@ function buildContainer(brotherList, selector){
   $(selector).append(html); //Only access dom once per container
 }
 
-buildContainer(seniors, "#seniors-container");
+// For a whole grade, with lil parallax container below
+function buildSection(section) {
+  var html = `
+  <div id="${section}" class="section white scrollspy">
+    <div class="row container">
+      <h2 class="header">${capitalizeFirstLetter(section)}</h2>
+      <div id="${section}-container">
+      </div>
+    </div>
+  </div>
+  <div class="parallax-container-mobile">
+    <div class="parallax-mobile"><img src="img/brothers/porch-pic-19.jpg"></div>
+  </div>
+  `;
+  $("#grades").append(html);
+}
 
-buildContainer(juniors, "#juniors-container");
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
 
-buildContainer(sophomores, "#sophomores-container");
+// TODO: Only show the sections if there are brothers for it
+if (seniors != null && seniors.length != 0) {
+  buildSection("seniors");
+  buildContainer(seniors, "#seniors-container");
+  $("#brothers-content").append(`<li><a href="#seniors">Seniors</a></li>`);
+}
 
-buildContainer(freshman, "#freshman-container");
+if (juniors != null && juniors.length != 0) {
+  buildSection("juniors");
+  buildContainer(juniors, "#juniors-container");
+  $("#brothers-content").append(`<li><a href="#juniors">Juniors</a></li>`);
+}
+
+if (sophomores != null && sophomores.length != 0) {
+  buildSection("sophomores");
+  buildContainer(sophomores, "#sophomores-container");
+  $("#brothers-content").append(`<li><a href="#sophomores">Sophomores</a></li>`);
+}
+if (freshman != null && freshman.length != 0) {
+  buildSection("freshman");
+  buildContainer(freshman, "#freshman-container");
+  $("#brothers-content").append(`<li><a href="#freshman">Freshman</a></li>`);
+}
 
 // Tenn ball
-$("#barrnp").hover( () => $("#barrnp").attr("src","img/portraits/BarrTennisBall.jpg"), () => $("#barrnp").attr("src","img/portraits/barrnp.jpg"));
+//$("#barrnp").hover( () => $("#barrnp").attr("src","img/portraits/BarrTennisBall.jpg"), () => $("#barrnp").attr("src","img/portraits/barrnp.jpg"));
--- js/site-components.js
+++ js/site-components.js
@@ -2,27 +2,47 @@
     -On re-election change contact links in footer
     -Add links to the hare in the header
 */
+const greek_letters = "&#916;&#931;&#934; &#918;&#923;";
+const president = "crabtrbm";
+const vp = "allenvj";
+const rushdick = "chois3";
+const webmaster = "purdydj";
+const presidentPhoneNumber = "(317)-285-8686";
+
+const pageList = `
+  <li><a href="/">Home</a></li>
+  <li><a href="brothers.html">Brothers</a></li>
+  <li><a href="officers.html">Officers</a></li>
+  <li><a href="rush.html">Rush</a></li>
+  <li><a href="history.html">History</a></li>
+  <li><a href="brotherhood.html">Brotherhood</a></li>
+  <li><a href="dogs.html">Dogs</a></li>
+`;
+
+const hareList = `
+  <li><a href="pdf/hare-spring-2008-2009.pdf">Spring 2008-09</a></li>
+  <li><a href="pdf/hare-fall-2009-2010.pdf">Fall 2009-10</a></li>
+  <li><a href="pdf/hare-winter-2009-2010.pdf">Winter 2009-10</a></li>
+  <li><a href="pdf/hare-spring-2009-2010.pdf">Spring 2009-10</a></li>
+  <li><a href="pdf/hare-fall-2010-2011.pdf">Fall 2010-11</a></li>
+  <li><a href="pdf/hare-winter-2010-2011.pdf">Winter 2010-11</a></li>
+  <li><a href="pdf/hare-spring-2012-2013.pdf">Spring 2012-13</a></li>
+  <li><a href="pdf/hare-spring-2013-2014.pdf">Spring 2013-14</a></li>
+  <li><a href="pdf/hare-winter-2015-2016.pdf">Winter 2015-16</a></li>
+  <li><a href="pdf/hare-winter-2016-2017.pdf">Winter 2016-17</a></li>
+  <li><a href="pdf/hare-spring-2016-2017.pdf">Spring 2016-17</a></li>
+  <li><a href="pdf/hare-fall-2017-2018.pdf">Fall 2017-18</a></li>
+  <li><a href="pdf/hare-winter-2017-2018.pdf">Winter 2017-18</a></li>
+  <li><a href="pdf/hare-fall-2018-2019.pdf">Fall 2018-19</a></li>
+  <li><a href="pdf/hare-spring-2018-2019.pdf">Spring 2018-19</a></li>
+  <li><a href="pdf/hare-fall-2019-2020.pdf">Fall 2019-20</a></li>
+  <li><a href="pdf/hare-winter-2019-2020.pdf">Winter 2019-20</a></li>
+  <li><a href="pdf/hare-fall-2020-2021.pdf">Fall 2020-21</a></li>
+`;
+
+
 class Header extends HTMLElement {
     connectedCallback() {
-      const hareList = `
-      <li><a href="pdf/hare-spring-2008-2009.pdf">Spring 2008-09</a></li>
-      <li><a href="pdf/hare-fall-2009-2010.pdf">Fall 2009-10</a></li>
-      <li><a href="pdf/hare-winter-2009-2010.pdf">Winter 2009-10</a></li>
-      <li><a href="pdf/hare-spring-2009-2010.pdf">Spring 2009-10</a></li>
-      <li><a href="pdf/hare-fall-2010-2011.pdf">Fall 2010-11</a></li>
-      <li><a href="pdf/hare-winter-2010-2011.pdf">Winter 2010-11</a></li>
-      <li><a href="pdf/hare-spring-2012-2013.pdf">Spring 2012-13</a></li>
-      <li><a href="pdf/hare-spring-2013-2014.pdf">Spring 2013-14</a></li>
-      <li><a href="pdf/hare-winter-2015-2016.pdf">Winter 2015-16</a></li>
-      <li><a href="pdf/hare-winter-2016-2017.pdf">Winter 2016-17</a></li>
-      <li><a href="pdf/hare-spring-2016-2017.pdf">Spring 2016-17</a></li>
-      <li><a href="pdf/hare-fall-2017-2018.pdf">Fall 2017-18</a></li>
-      <li><a href="pdf/hare-winter-2017-2018.pdf">Winter 2017-18</a></li>
-      <li><a href="pdf/hare-fall-2018-2019.pdf">Fall 2018-19</a></li>
-      <li><a href="pdf/hare-spring-2018-2019.pdf">Spring 2018-19</a></li>
-      <li><a href="pdf/hare-fall-2019-2020.pdf">Fall 2019-20</a></li>
-      <li><a href="pdf/hare-winter-2019-2020.pdf">Winter 2019-20</a></li>
-      <li><a href="pdf/hare-fall-2020-2021.pdf">Fall 2020-21</a></li>`;
 
       this.innerHTML = `
         <header>
@@ -31,27 +51,16 @@ class Header extends HTMLElement {
           </ul>
           <nav class="green darken-2">
             <div class="nav-wrapper">
-              <a href="index.html" class="brand-logo hide-on-med-and-down" style="">&#916;&#931;&#934; &#918;&#923;</a>
-              <span class="brand-logo hide-on-large-only center">&#916;&#931;&#934; &#918;&#923;</span>
+              <a href="/" class="brand-logo hide-on-med-and-down" style="">${greek_letters}</a>
+              <span class="brand-logo hide-on-large-only center">${greek_letters}</span>
               <a href="" data-activates="mobile-menu" class="button-collapse"><i class="mdi-navigation-menu"></i></a>
               <ul class="right desktop-nav hide-on-med-and-down">
-                <li><a href="brothers.html">Brothers</a></li>
-                <li><a href="officers.html">Officers</a></li>
-                <li><a href="rush.html">Rush</a></li>
-                <li><a href="history.html">History</a></li>
-                <li><a href="brotherhood.html">Brotherhood</a></li>
-                <li><a href="dogs.html">Dogs</a></li>
+                ${pageList}
                 <li><a class="dropdown-button" href="" data-activates="dropdown-hares">Hares Newsletter<i
                       class="mdi-navigation-arrow-drop-down right"></i></a></li>
               </ul>
               <ul class="side-nav" id="mobile-menu">
-                <li><a href="index.html">Home</a></li>
-                <li><a href="brothers.html">Brothers</a></li>
-                <li><a href="officers.html">Officers</a></li>
-                <li><a href="rush.html">Rush</a></li>
-                <li><a href="history.html">History</a></li>
-                <li><a href="brotherhood.html">Brotherhood</a></li>
-                <li><a href="dogs.html">Dogs</a></li>
+                ${pageList}
                 <ul class="collapsible" data-collapsible="accordion">
                   <li>
                     <div class="collapsible-header green-text text-darken-3">Hares Newsletter<i
@@ -73,12 +82,6 @@ class Header extends HTMLElement {
 
 class Footer extends HTMLElement {
   connectedCallback(){
-    const president = "theneltj";
-    const vp = "purdydj";
-    const rushdick = "crabtrbm";
-    const webmaster = "chungy";
-    const presidentPhoneNumber = "(973)-936-0076";
-    
     this.innerHTML = `
     <footer class="page-footer">
       <div class="container">
@@ -116,7 +119,7 @@ class Footer extends HTMLElement {
       </div>
       <div class="footer-copyright">
         <div class="container">
-          &copy; 2021 &#916;&#931;&#934; &#918;&#923; &bull; 429 S. 6<sup>th</sup> St. &bull; Terre Haute, IN 47807 &bull;
+          &copy; 2021 ${greek_letters} &bull; 429 S. 6<sup>th</sup> St. &bull; Terre Haute, IN 47807 &bull;
           ${presidentPhoneNumber}
         </div>
       </div>
